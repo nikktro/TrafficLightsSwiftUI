4cef54badfc488fbc45e97b476a4fb967ea87df7
Nikolay Trofimov <nikktro@gmail.com>
2022-06-23 22:32:42 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Nikolay Trofimov on 23.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
