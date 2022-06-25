58978f56690b6969a52f6327cac0d5f400fb34e2
Nikolay Trofimov <nikktro@gmail.com>
2022-06-25 21:56:41 +0300

    ButtonView and ContentView code refactoring

--- TrafficLights/ButtonView.swift
+++ TrafficLights/ButtonView.swift
@@ -8,21 +8,27 @@
 import SwiftUI
 
 struct ButtonView: View {
-    let buttonText: String
+    let title: String
+    let action: () -> Void
     
     var body: some View {
-        Text(buttonText)
-            .frame(width: 200, height: 50)
-            .background(Color.blue)
-            .font(.title.bold())
-            .foregroundColor(.white)
-            .cornerRadius(15)
-            .overlay(RoundedRectangle(cornerRadius: 15).stroke(Color.white, lineWidth: 3))
+        Button(action: action) {
+            Text(title)
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(.white)
+        }
+        .frame(width: 200, height: 50)
+        .background(Color.blue)
+        .cornerRadius(15)
+        .overlay(
+            RoundedRectangle(cornerRadius: 15)
+                .stroke(Color.white, lineWidth: 3))
     }
 }
 
 struct ButtonView_Previews: PreviewProvider {
     static var previews: some View {
-        ButtonView(buttonText: "START")
+        ButtonView(title: "START", action: {})
     }
 }
--- TrafficLights/ContentView.swift
+++ TrafficLights/ContentView.swift
@@ -8,54 +8,41 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var redOpacity = 0.3
-    @State private var yellowOpacity = 0.3
-    @State private var greenOpacity = 0.3
-    @State private var setColor: Color = .red
-    @State private var buttonText = "START"
+    //MARK: - Private Properties
+    @State private var setColor: Color = .clear
+    @State private var buttonTitle = "START"
     
+    //MARK: - Private Methods
+    private func changeLightColor() {
+        switch setColor {
+            case .red: setColor = .yellow
+            case .yellow: setColor = .green
+            default: setColor = .red
+        }
+    }
+}
+
+extension ContentView {
     var body: some View {
         ZStack {
             Color(.black)
                 .ignoresSafeArea()
             
             VStack(spacing: 20) {
-                SignalLightView(color: .red, opacity: redOpacity)
-                SignalLightView(color: .yellow, opacity: yellowOpacity)
-                SignalLightView(color: .green, opacity: greenOpacity)
+                SignalLightView(color: .red, opacity: setColor == .red ? 1 : 0.3)
+                SignalLightView(color: .yellow, opacity: setColor == .yellow ? 1 : 0.3)
+                SignalLightView(color: .green, opacity: setColor == .green ? 1 : 0.3)
+                
                 Spacer()
-                Button(action: { buttonPressed() }) {
-                    ButtonView(buttonText: buttonText)
+                
+                ButtonView(title: buttonTitle) {
+                    if buttonTitle == "START" {
+                        buttonTitle = "NEXT"
+                    }
+                    changeLightColor()
                 }
             }
             .padding([.top, .bottom], 40)
-        }
-    }
-    
-    //MARK: - Private Methods
-    private func buttonPressed() {
-        buttonText = "NEXT"
-        dimLightColor()
-        changeLightColor()
-    }
-    
-    private func dimLightColor() {
-        redOpacity = 0.3
-        yellowOpacity = 0.3
-        greenOpacity = 0.3
-    }
-    
-    private func changeLightColor() {
-        switch setColor {
-        case .red:
-            redOpacity = 1
-            setColor = .yellow
-        case .yellow:
-            yellowOpacity = 1
-            setColor = .green
-        default:
-            greenOpacity = 1
-            setColor = .red
         }
     }
 }
